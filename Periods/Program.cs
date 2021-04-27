9be68b902dca5249aab88dcc9554d24c267fa9ba
Maks <maxek13@interia.pl>
2021-04-27 17:00:11 +0200

    Add all files

---
+++ Periods/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Periods
+{
+    public class Period
+    {
+        public int Start { get; }
+        public int Stop { get; }
+
+        public Period(int start, int stop)
+        {
+            Start = start;
+            Stop = stop;
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<Period> //Creating list of periods
+            {
+                new Period(1, 3),
+                new Period(1, 4),
+                new Period(2, 3),
+                new Period(5, 7),
+                new Period(17, 23),
+                new Period(15, 20),
+                new Period(24, 27),
+                new Period(25, 30)
+            };
+
+            list.Sort((x, y) => x.Start.CompareTo(y.Start)); //sorting list of periods
+
+            var stack = new Stack<Period>();
+
+            foreach (var CurrentStack in list) //Pushing list into stack merging overlaping periods
+            {
+                switch (stack.Count)             
+                {
+                    case 0:
+                        stack.Push(CurrentStack);
+                        break;
+
+                    default:
+                
+                        var LastStack = stack.Peek();
+
+                        if (LastStack.Stop >= CurrentStack.Start && LastStack.Stop <= CurrentStack.Stop)
+                        {
+                            int popedPeriod = LastStack.Start;
+                            stack.Pop();
+                            stack.Push(new Period(popedPeriod, CurrentStack.Stop));
+                        }
+                        if (LastStack.Stop >= CurrentStack.Start && LastStack.Stop > CurrentStack.Stop)
+                        {
+                            break;
+                        }
+                        else if (LastStack.Stop < CurrentStack.Start)
+                        {
+                            stack.Push(CurrentStack);
+                        }
+                        break;
+                }
+            }
+            list.Clear(); //cleaing the list
+
+            foreach (var period in stack) //Adding periods from list to stack
+            {
+                list.Add(period);
+            }
+
+            list.Reverse(); //Reversing the list
+
+            foreach (var period in list) //Displaying list of periods
+            {
+                Console.WriteLine(period.Start + " " + period.Stop);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Periods/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("Periods")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Periods")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("9bfd117c-3d5e-4357-9d51-f8ecd90e55c1")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Poprawka
+//
+// Możesz określić wszystkie wartości lub użyć domyślnych numerów kompilacji i poprawki
+// przy użyciu symbolu „*”, tak jak pokazano poniżej:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
